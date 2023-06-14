90ddacdbab327dd4a6dae352d9c5da78881d21a2
Jacky Tung <jackytung20@gmail.com>
2023-06-14 17:47:58 -0400

    Modified assignment files

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get the grid element
 const grid = document.getElementById("grid");
-var globalColor="red";
+var globalColor = "";
 
 function addRow() {
   getSelectedColor();
@@ -15,11 +15,14 @@ function addRow() {
   for (let i = 0; i < numColumns; i++) {
     const newCell = document.createElement("span");
     newCell.classList.add("cell");
-    newCell.innerText="Cell"
-    newCell.style.background=globalColor;
-    newCell.onclick="changeColor()";
+    newCell.innerText = "Cell";
+    newCell.style.background = globalColor;
+    newCell.addEventListener('click', (event) => {
+      const cells = event.target;
+      getSelectedColor();
+      cells.style.background = globalColor;
+    });
     newRow.appendChild(newCell);
-
   }
 
   // Append the new row to the grid
@@ -35,34 +38,55 @@ function addColumn() {
   rows.forEach((row) => {
     const newCell = document.createElement("span");
     newCell.classList.add("cell");
-    newCell.innerText="Cell";
-    newCell.style.background=globalColor;
+    newCell.innerText = "Cell";
+    newCell.style.background = globalColor;
+    newCell.addEventListener("click", (event) => {
+      const cells = event.target;
+      getSelectedColor();
+      cells.style.background = globalColor;
+    });
     row.appendChild(newCell);
   });
-
 }
 
-function removeRow(){
-  const row=document.getElementsByClassName("row")[0];
+function removeRow() {
+  const row = document.getElementsByClassName("row")[0];
   row.remove();
+}
 
-};
-
-function removeColumn(){
-  //go through each row and 
-  const row=document.getElementsByClassName("row");
-  for (let item of row){
-    let itemtoRemove=item.lastChild;
+function removeColumn() {
+  //go through each row and
+  const row = document.getElementsByClassName("row");
+  for (let item of row) {
+    let itemtoRemove = item.lastChild;
     itemtoRemove.remove();
   }
-};
+}
 
 function getSelectedColor() {
   const colorDropdown = document.getElementById("colorDropdown");
   const selectedColor = colorDropdown.value;
-  globalColor=selectedColor;
+  globalColor = selectedColor;
 }
 
-function changeColor(){
-  
+// fill all uncolored cells with the currently selected color
+function fillUncolored(){
+  getSelectedColor();
+  const cells = document.getElementsByClassName("cell");
+  for(let cell of cells){
+    if(cell.style.background === "")
+      cell.style.background = globalColor;
+  }
 }
+
+// fill all cells with the currently selected color
+function fillAll(){
+  getSelectedColor();
+  const cells = document.getElementsByClassName("cell");
+  for (let cell of cells) {
+    cell.style.background = globalColor;
+  }
+}
+
+// clear all cells/restore all cells to their original/initial color
+
