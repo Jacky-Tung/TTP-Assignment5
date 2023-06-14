1960505475d6103a97805d5f3f3bc4e56bdd4400
Jacky Tung <jackytung20@gmail.com>
2023-06-14 18:41:47 -0400

    Completed all features

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // Get the grid element
 const grid = document.getElementById("grid");
 var globalColor = "";
-var event= new Event("");
+var isMouseDown = false;
 
+document.addEventListener("mousedown", function () {
+  isMouseDown = true;
+});
+
+document.addEventListener("mouseup", function () {
+  isMouseDown = false;
+});
+
+// add rows to the grid
 function addRow() {
   getSelectedColor();
   // Create a new row element
@@ -16,26 +25,25 @@ function addRow() {
   for (let i = 0; i < numColumns; i++) {
     const newCell = document.createElement("span");
     newCell.classList.add("cell");
-    newCell.value=globalColor;
+    newCell.value = globalColor;
     newCell.innerText = "Cell";
     newCell.style.background = globalColor;
 
-    // newCell.addEventListener('click', (event) => {
-    //   const cells = event.target;
-    //   getSelectedColor();
-    //   cells.style.background = globalColor;
-    // });
-    newCell.addEventListener("mousedown",(event)=>{
-      // const cells = event.target;
-      // getSelectedColor();
-      // cells.style.background = globalColor;
-    })
-    // cell.addEventListener("mouseover", (event) => {
-    //   const cells = event.target;
-    //   getSelectedColor();
-    //   cells.style.background = globalColor;
-    // });
-    
+    // click on a single cell, changing its color to the currently selected color
+    newCell.addEventListener("click", (event) => {
+      const cells = event.target;
+      getSelectedColor();
+      cells.style.background = globalColor;
+    });
+    // click and hold (mouseover) from a single cell (start) to a different cell (end) such that all affected/hovered-over cells from start to end change to the currently selected color
+    newCell.addEventListener("mouseover", (event) => {
+      if (isMouseDown) {
+        const cells = event.target;
+        getSelectedColor();
+        cells.style.background = globalColor;
+      }
+    });
+
     newRow.appendChild(newCell);
   }
 
@@ -43,6 +51,7 @@ function addRow() {
   grid.appendChild(newRow);
 }
 
+//  add columns to the grid
 function addColumn() {
   getSelectedColor();
   // Get all existing rows in the grid
@@ -51,39 +60,38 @@ function addColumn() {
   // Create a new cell for each row
   rows.forEach((row) => {
     const newCell = document.createElement("span");
-    newCell.value=globalColor;
+    newCell.value = globalColor;
     newCell.classList.add("cell");
     newCell.innerText = "Cell";
     newCell.style.background = globalColor;
 
+    // click on a single cell, changing its color to the currently selected color
     newCell.addEventListener("click", (event) => {
       const cells = event.target;
       getSelectedColor();
       cells.style.background = globalColor;
     });
 
-    newCell.addEventListener("onmousedown",(event)=>{
-      const cells = event.target;
-      getSelectedColor();
-      cells.style.background = globalColor;
-    })
-
-
+    // click and hold (mouseover) from a single cell (start) to a different cell (end) such that all affected/hovered-over cells from start to end change to the currently selected color
     newCell.addEventListener("mouseover", (event) => {
-      const cells = event.target;
-      getSelectedColor();
-      cells.style.background = globalColor;
+      if (isMouseDown) {
+        const cells = event.target;
+        getSelectedColor();
+        cells.style.background = globalColor;
+      }
     });
 
     row.appendChild(newCell);
   });
 }
 
+// remove rows from the grid
 function removeRow() {
   const row = document.getElementsByClassName("row")[0];
   row.remove();
 }
 
+// remove columns from the grid
 function removeColumn() {
   //go through each row and
   const row = document.getElementsByClassName("row");
@@ -93,6 +101,7 @@ function removeColumn() {
   }
 }
 
+// select a color from a dropdown menu of colors
 function getSelectedColor() {
   const colorDropdown = document.getElementById("colorDropdown");
   const selectedColor = colorDropdown.value;
